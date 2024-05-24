2ce92205c20b051bc6feb93ea5e1fa11cf6c4d76
Chris Kelly <eurochriskelly@gmail.com>
2024-05-24 21:57:27 +0200

    Re-configure storybook for use directly within this repo
    Previously it was set up as an import. Now it can be used to test the
    components here

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,11 @@
-// main.js
 const path = require('path');
 
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
   enableCrashReporter: false,
   addons: [
     "@storybook/addon-links",
@@ -26,57 +28,53 @@ module.exports = {
   },
   webpackFinal: async (config) => {
     // Resolve aliases
-    const p1 = path.resolve(__dirname, '../src');
-    const p2 = path.resolve(__dirname, '../node_modules/gcp-core-fe');
     config.resolve.alias = {
       ...config.resolve.alias,
-      '~': p1,
-      '^': p2 
+      '~': path.resolve(__dirname, '../'), // This should point to the root of your project
     };
 
-  const scssRuleIndex = config.module.rules.findIndex(rule => rule.test && rule.test.toString().includes('scss'));
-  if (scssRuleIndex !== -1) {
-    // Update existing SCSS rule to enable CSS modules
-    config.module.rules[scssRuleIndex] = {
-      test: /\.module\.scss$/, // Target only .module.scss files for CSS modules
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true, // Enable CSS modules
-            localIdentName: '[name]__[local]___[hash:base64:5]', // Optional: configure generated class names
+    const scssRuleIndex = config.module.rules.findIndex(rule => rule.test && rule.test.toString().includes('scss'));
+    if (scssRuleIndex !== -1) {
+      // Update existing SCSS rule to enable CSS modules
+      config.module.rules[scssRuleIndex] = {
+        test: /\.module\.scss$/, // Target only .module.scss files for CSS modules
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true, // Enable CSS modules
+              localIdentName: '[name]__[local]___[hash:base64:5]', // Optional: configure generated class names
+            },
           },
-        },
-        'sass-loader',
-      ],
-      include: [
-        path.resolve(__dirname, '../src'),
-        path.resolve(__dirname, '../node_modules/gcp-core-fe/src/css'),
-      ]
-    };
-  } else {
-    // Add new rule for SCSS files if not exists, with CSS modules enabled
-    config.module.rules.push({
-      test: /\.module\.scss$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            localIdentName: '[name]__[local]___[hash:base64:5]',
+          'sass-loader',
+        ],
+        include: [
+          path.resolve(__dirname, '../src'),
+          path.resolve(__dirname, '../node_modules/gcp-core-fe/src/css'),
+        ]
+      };
+    } else {
+      // Add new rule for SCSS files if not exists, with CSS modules enabled
+      config.module.rules.push({
+        test: /\.module\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
           },
-        },
-        'sass-loader',
-      ],
-      include: [
-        path.resolve(__dirname, '../src'),
-        path.resolve(__dirname, '../node_modules/gcp-core-fe/src/css'),
-      ]
-    });
-  }
-
+          'sass-loader',
+        ],
+        include: [
+          path.resolve(__dirname, '../src'),
+          path.resolve(__dirname, '../node_modules/gcp-core-fe/src/css'),
+        ]
+      });
+    }
 
     // Check for JavaScript/JSX rule and update if necessary
     const jsRule = config.module.rules.find(rule =>
